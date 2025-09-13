8a08aa684090b2190d58d27cd74b3edc05c2ac89
Abhi Singh <156217394+urfatherabhi@users.noreply.github.com>
2025-09-13 11:23:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "09bd2e64c04dc911d0bc45648d236fab"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "7556244377"
-SUPPORT_GRP = "@imagine_iq"
-UPDATE_CHNL = "@imagine_iq"
+SUPPORT_GRP = "imagine_iq"
+UPDATE_CHNL = "imagine_iq"
 OWNER_USERNAME = "URFather_ABHI"
 
